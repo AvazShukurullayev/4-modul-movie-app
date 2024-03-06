8e45743a90677dffe439f5c8df71a0fddc56083a
Avaz <elonarm21@gmail.com>
2024-03-06 23:02:45 +0500

    npm install react-responsive-modal and eslint off

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import "./components/styles/index.scss"
-import App from "./components/app/App.jsx";
+import "./styles/index.scss"
+import App from "./components/app/app.jsx";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
