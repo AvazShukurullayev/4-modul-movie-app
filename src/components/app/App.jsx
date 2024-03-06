2366e2241e17256c558f99be12d971ab09ca8d7e
Avaz <elonarm21@gmail.com>
2024-03-07 00:35:30 +0500

    movie services

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,15 +1,32 @@
 import Header from '../header/header'
 import Hero from '../hero/hero'
 import RowMovies from '../row-movies/row-movies'
+import MovieService from "../../services/movie-service.js";
 
 const App = () => {
-	return (
-		<div className='app'>
-			<Header />
-			<Hero />
-			<RowMovies />
-		</div>
-	)
+    const movieSeries = new MovieService()
+    console.log(movieSeries)
+    // promise qaytaradi
+    movieSeries.getPopularMovies()
+        .then(data => console.log("getPopularMovies => ",data))
+        .catch((err) => console.log(err))
+        .finally(() => console.log("Finally"))
+
+    movieSeries.getTrandingMovies()
+        .then(data => console.log("getTrandingMovies => ",data))
+        .catch((err) => console.log(err))
+        .finally(() => console.log("Finally"))
+    movieSeries.getDetailed(21)
+        .then(data => console.log("getDetailed => ",data))
+        .catch((err) => console.log(err))
+        .finally(() => console.log("Finally"))
+    return (
+        <div className='app'>
+            <Header/>
+            <Hero/>
+            <RowMovies/>
+        </div>
+    )
 }
 
 export default App
---
+++ src/services/movie-service.js
@@ -0,0 +1,35 @@
+class MovieService {
+    _apiBase = 'https://api.themoviedb.org/3/movie'
+    _apiLng = 'language=en-US'
+    getResources = async (url) => {
+        // bu universal method boldi bu method bizga yo xato qaytardi yoki javob{} qaytaradi
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OTAzMzg1ZmQ5YzQxMWYxZGJmNzM5M2FhNGFiYTZkNiIsInN1YiI6IjY1ZThiMGMwOTYzODY0MDE0NmM4NDYwYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DnWH9dvNYILPto9IQnwrW7mEBtiwPloHN6mLizKVsRE'
+            }
+        })
+        if (!response.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${response.status}`)
+        }
+        return await response.json()
+    }
+
+    getPopularMovies = async () => {
+        // shu method orqali universal method ga murojaat qilib yoki xato olamiz yoki javob {}
+        // method dagi url ko'p takrorlanvotti shuni alohida ozgaruvchiga olib chiqamiz
+        return this.getResources(`${this._apiBase}/popular?${this._apiLng}&api_key=4903385fd9c411f1dbf7393aa4aba6d6`)
+    }
+
+    getTrandingMovies = async () => {
+        return this.getResources(`${this._apiBase}/top_rated?${this._apiLng}&api_key=4903385fd9c411f1dbf7393aa4aba6d6`)
+    }
+
+    getDetailed = async (id) => {
+        return this.getResources(`${this._apiBase}/${id}?${this._apiLng}&api_key=4903385fd9c411f1dbf7393aa4aba6d6`)
+    }
+
+}
+
+export default MovieService
