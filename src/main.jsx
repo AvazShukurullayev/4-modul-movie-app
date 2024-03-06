944fa35a8a26748cd5e4c720e27172a7effe03f5
Avaz <elonarm21@gmail.com>
2024-03-06 21:40:16 +0500

    first config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import "./components/styles/index.scss"
+import App from "./components/app/App.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
