70b1793814b241eca7b7fc029360b441cd381306
Avaz <elonarm21@gmail.com>
2024-03-15 20:51:07 +0500

    added lazy function and suspense to project to optimize

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,24 +1,28 @@
-import { Route, Routes } from 'react-router-dom'
-import DetailedPage from '../../pages/detailed-page'
-import HomePage from '../../pages/home-page'
-import NotFoundPage from '../../pages/not-found-page'
-import PopularPage from '../../pages/popular-page'
-import TrandingPage from '../../pages/tranding-page'
+import {lazy, Suspense} from "react"
+import {Route, Routes} from 'react-router-dom'
 import Navbar from '../navbar/navbar'
+import Spinner from "../spinner/spinner.jsx";
 
+const HomePage = lazy(() => import("../../pages/home-page"))
+const TrendingPage = lazy(() => import("../../pages/trending-page.jsx"))
+const PopularPage = lazy(() => import("../../pages/popular-page"))
+const DetailedPage = lazy(() => import("../../pages/detailed-page"))
+const NotFoundPage = lazy(() => import("../../pages/not-found-page"))
 const App = () => {
-	return (
-		<div className='app'>
-			<Navbar />
-			<Routes>
-				<Route path='/' element={<HomePage />} />
-				<Route path='/tranding' element={<TrandingPage />} />
-				<Route path='/popular' element={<PopularPage />} />
-				<Route path='/movie/:movieId' element={<DetailedPage />} />
-				<Route path='*' element={<NotFoundPage />} />
-			</Routes>
-		</div>
-	)
+    return (
+        <div className='app'>
+            <Navbar/>
+            <Suspense fallback={<Spinner/>}>
+                <Routes>
+                    <Route path='/' element={<HomePage/>}/>
+                    <Route path='/tranding' element={<TrendingPage/>}/>
+                    <Route path='/popular' element={<PopularPage/>}/>
+                    <Route path='/movie/:movieId' element={<DetailedPage/>}/>
+                    <Route path='*' element={<NotFoundPage/>}/>
+                </Routes>
+            </Suspense>
+        </div>
+    )
 }
 
 export default App
--- src/pages/trending-page.jsx
+++ src/pages/trending-page.jsx
@@ -1,7 +1,7 @@
 import RowMovies from '../components/row-movies/row-movies'
 
-const TrandingPage = () => {
+const TrendingPage = () => {
 	return <RowMovies />
 }
 
-export default TrandingPage
+export default TrendingPage
